e0a1b79d1efa3b244bda68bb6f59e8d1a6c7e22d
Henry Daunt <116744334+daunt-henry@users.noreply.github.com>
2025-06-29 17:51:43 +0530

    Create MotionEquation_PS_02_04.java

---
+++ MotionEquation_PS_02_04.java
@@ -0,0 +1,24 @@
+/* 
+Write the following expression in a java program:
+
+V²- u² 2 245 
+*/
+
+public class MotionEquation {
+    public static void main(String[] args) {
+        // Given values
+        int a = 2;
+        int s = 245;
+
+        // Calculate v² - u² = 2 * a * s
+        int result = 2 * a * s;
+
+        // Display the result
+        System.out.println("The value of v² - u² is: " + result);
+    }
+}
+
+/*
+output 
+The value of v² - u² is: 980
+*/
