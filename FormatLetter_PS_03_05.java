16ea9958b7bc2cfd3aeb6eee58c89209f4144e60
Henry Daunt <116744334+daunt-henry@users.noreply.github.com>
2025-06-29 19:03:21 +0530

    Create FormatLetter_PS_03_05.java

---
+++ FormatLetter_PS_03_05.java
@@ -0,0 +1,25 @@
+/*
+Write a program to format the following letter using escape sequence Characters.
+
+letter = "Dear Harry, This Java Course is nice. Thanks"
+
+*/
+
+public class FormatLetter {
+    public static void main(String[] args) {
+        // Original letter formatted using escape sequences
+        String letter = "Dear Harry,\n\tThis Java Course is nice.\nThanks";
+
+        // Display the formatted letter
+        System.out.println(letter);
+    }
+}
+
+
+/*
+
+Dear Harry,
+	This Java Course is nice.
+Thanks
+
+*/
